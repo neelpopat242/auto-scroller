ad07150178450f4d1c1f9e09c17bc3e7ba0103ad
Neel <neel@betterhalf.ai>
2024-12-15 14:55:06 +0530

    update the setup.py file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_scroller",
-    version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    version="0.1.1",
+    author="Neel Popat",
+    author_email="neelpopat242@gmail.com",
     description="A package for detecting hand gestures to control scrolling.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/auto_scroller",  # Update with your repo URL
+    url="https://github.com/neelpopat242/auto_scroller",  # Update with your repo URL
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines(),
     classifiers=[
